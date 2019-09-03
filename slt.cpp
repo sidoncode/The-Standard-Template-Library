3f4fca847b1976b0fa294fe2fa5f0a767382be47
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-09-03 23:43:30 +0530

    Documentaton of the statement

--- slt.cpp
+++ slt.cpp
@@ -8,6 +8,9 @@ using namespace std;
 
 /*int main(){
   //sorting algorithm
+  //Here the number userdefined is 10
+  //if it is not defined as 10 then we have to manually insert the number;
+  //like- int a[]  ={1,5,16,32,6,8,26,46,26,246};{after this statement the "sizeof" operator will be encounter}
 int a[10];
 for(int i=0;i<10;i++){
     cin>> a[i];}
@@ -40,7 +43,7 @@ inline void strlenfx(){
     string s = "Example";
     int i;
     int len;
-    for(i =0;s[i];i++){
+    for(i=0;s[i];i++){
            
     }
 }
