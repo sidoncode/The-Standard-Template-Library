a49dfc729b8c128feba9ee15d8d0f44d74d24b9e
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-09-03 22:14:49 +0530

    sort algo

--- slt.cpp
+++ slt.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
+
 #include<conio.h>
+#include<algorithm>
 using namespace std;
 int main(){
-    cout<< "hello"<< endl;
+    int a[10] = {9,8,7,6,5};
+    cout<< sort(a,8)<<endl;
+
 getch();
 return 0;
 }
+//C:\Users\Sidd\Desktop\GITHUB\stl github\The-Standard-Template-Library>g++ slt.cpp -o slt.exe
