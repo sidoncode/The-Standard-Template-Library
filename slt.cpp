72eafaf1352ea1c8253ce21df0b9bfb314eabdf4
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-09-03 22:19:37 +0530

    change in algo

--- slt.cpp
+++ slt.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
-
 #include<conio.h>
 #include<algorithm>
 using namespace std;
 int main(){
-    int a[10] = {9,8,7,6,5};
-    cout<< sort(a,8)<<endl;
-
+  int a;
+  cin>> a;
+  cout<<a;
 getch();
-return 0;
-}
+return 0;}
 //C:\Users\Sidd\Desktop\GITHUB\stl github\The-Standard-Template-Library>g++ slt.cpp -o slt.exe
