a2e51e333456275e2b02409fa4a990a28a5305c8
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-09-17 21:43:09 +0530

    new cpp file

---
+++ class_and_objects.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<conio.h>
+#include<algorithm>
+
+using namespace std;
+
+class example{
+private:
+    int a;
+public:
+    example(){
+        a = 5;
+    }
+    void assign(){
+        cout<< a<<endl;
+    }
+};
+int main(){
+void example :: assign();
+return 0;
+getch();
+}
