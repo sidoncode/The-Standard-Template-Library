3da575ec02adf0236170baee49de2b4d360e229a
Siddharth Sharma <40432616+sidoncode@users.noreply.github.com>
2019-09-24 23:18:28 +0530

    destructor

--- class_and_objects.cpp
+++ class_and_objects.cpp
@@ -8,13 +8,22 @@ class example{
 private:
     int a;
 public:
-    
+    ~example(){
+        int a = 5;
+        cout<<"The Value of a is"<<a<<endl;
+    }
+    void disp(){
+        cout<<"The Value of a is "<<a<<endl;
+    }
 };
+//Here the example of the destructor which destroys itself when the data is passed.
+//so the a = 3 is the garbage value which is displayed
 
 int main(){
     example e;
-    e.example();
     e.~example();
+    cout<<"Below is the executed fx of disp();"<<endl;
+    e.disp();
     getch();
 return 0;
 }
